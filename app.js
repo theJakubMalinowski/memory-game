ada7fce7c8ac789c6a436086564ea3b22dbe38ce
Jakub <malinowski.jakub@yahoo.com>
2023-01-26 20:44:18 -0600

    Cards flip on click displaying images

--- app.js
+++ app.js
@@ -56,16 +56,26 @@ document.addEventListener('DOMContentLoaded', () => {
   cardArray.sort(()=> 0.5 - Math.random())
   const gridDisplay = document.querySelector('#grid')
   console.log(gridDisplay)
+  const cardsChosen = []
 
   // Create "for" loop
 
   function createBoard(){
-    for (let i=0; i < 10; i++){
+    for (let i=0; i < cardArray.length; i++){
       const card = document.createElement('img')
       card.setAttribute('src', 'images/blank.png')
       card.setAttribute('data-id', i)
+      card.addEventListener('click',flipCard)
       gridDisplay.appendChild(card)
     }
   }
   createBoard()
+  function flipCard(){
+  const cardId = this.getAttribute('data-id')
+  console.log(cardArray[cardId].name)
+  cardsChosen.push(cardArray[cardId].name)
+  console.log('clicked')
+  console.log(cardsChosen)
+  this.setAttribute('src', cardArray[cardId].img)
+  }
 });
